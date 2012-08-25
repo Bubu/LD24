3ef0e5f1d2469159be9ce99b50f20901d5192043
Marcus Hoffmann <bubuiic@aol.com>
2012-08-25 17:30:08 +0200

    class structure

--- game.py
+++ game.py
@@ -9,48 +9,49 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-def run():
-    while True:
-        for event in pygame.event.get():
+# mouse buttons
+LEFT = 1
+MIDDLE = 2
+RIGHT = 3
+
+class game:
+    
+    def __init__(self):
+        # set up pygame
+        pygame.init()
+        
+        # set up the window
+        self.screen = pygame.display.set_mode((800, 600), 0, 32)
+        pygame.display.set_caption('Human Evolution!')
+
+        # set up fonts
+        basicFont = pygame.font.Font('res' + os.sep + 'EBGaramond08-Regular.ttf', 30)
+
+        # draw the white background onto the surface
+        self.screen.fill(BLACK)
+
+        # set up the text
+        text = basicFont.render('Once upon a time on our blue planet...', True, WHITE)
+        textRect = text.get_rect()
+        textRect.centerx = self.screen.get_rect().centerx
+        textRect.centery = self.screen.get_rect().centery
+        # draw the text onto the surface
+        self.screen.blit(text, textRect)
+
+    def run(self):
+        while True:
+            event = pygame.event.wait()
             if event.type == QUIT:
                 return
             if event.type == KEYUP and event.key == K_ESCAPE:
                 return
+            if event.type == MOUSEBUTTONUP and event.button == LEFT:
+                self.screen.fill(BLACK)
+            pygame.display.update()
 
-def init():
-    # set up pygame
-    pygame.init()
-    
-    # set up the window
-    windowSurface = pygame.display.set_mode((800, 600), 0, 32)
-    pygame.display.set_caption('Human Evolution!')
+    def close(self):
+        pygame.quit()
 
-    # set up fonts
-    basicFont = pygame.font.Font('res' + os.sep + 'EBGaramond08-Regular.ttf', 30)
-
-    # draw the white background onto the surface
-    windowSurface.fill(BLACK)
-
-    # set up the text
-    text = basicFont.render('Once upon a time on our blue planet...', True, WHITE)
-    textRect = text.get_rect()
-    textRect.centerx = windowSurface.get_rect().centerx
-    textRect.centery = windowSurface.get_rect().centery
-    # draw the text onto the surface
-    windowSurface.blit(text, textRect)
-
-    # draw the window onto the screen
-    pygame.display.update()
-
-
-
-# draw a green polygon onto the surface
-#pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
-
-# draw some blue lines onto the surface
-#pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4)
-#pygame.draw.line(windowSurface, BLUE, (120, 60), (60, 120))
-#pygame.draw.line(windowSurface, BLUE, (60, 120), (120, 120), 4)
 
 # draw a blue circle onto the surface
 #pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
@@ -58,16 +59,8 @@ def init():
 # draw a red ellipse onto the surface
 #pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
 
-# draw the text's background rectangle onto the surface
-#pygame.draw.rect(windowSurface, RED, (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
-
-# get a pixel array of the surface
-#pixArray = pygame.PixelArray(windowSurface)
-#pixArray[480][380] = BLACK
-#del pixArray
 
 
-
-init()
-run()
-pygame.quit()
+aGame = game()
+aGame.run()
+aGame.close()
